c8b9007d7d688d6ac3ff390e0b1db0c72b9e9a79
Vladislav Tereshkin <firstaake@gmail.com>
2020-06-03 22:14:34 +0300

    restore session with db

--- server/controllers/restoreSession.js
+++ server/controllers/restoreSession.js
@@ -1,31 +1,29 @@
-const path = require('path');
 const createError = require('http-errors');
-
-const USERS_FILE = path.join(__dirname, '../database/users/users.json');
-const readFrom = require('../utils/readFromFile');
+const User = require('../models/user');
 
 const restoreSession = (req, res, next) => {
   if (req.cookies.UID) {
-    readFrom(USERS_FILE)
-        .then((data) => {
-          const users = JSON.parse(data);
-          const user = users.find((item) => item.access_token === req.cookies.UID);
+    User.findOne({access_token: req.cookies.UID}, function(err, user) {
+      if (err) {
+        return next(createError(500, 'Server error!'));
+      }
 
-          if (user) {
-            delete user.access_token;
-            delete user.password;
-            return res.json(user);
-          }
-          next(createError(200, 'Wrong session!'));
-        })
-        .catch((err) => {
-          console.error(err);
-          return next(
-              createError(500, 'Server error, please try again later...'),
-          );
-        });
+      if (user) {
+        const returnedUser = {
+          name: user.name,
+          phone: user.phone,
+          email: user.email,
+          avatar: user.avatar,
+          bookings: [...user.bookings],
+        };
+
+        res.send(returnedUser);
+      } else {
+        res.send({});
+      }
+    });
   } else {
-    next(createError(200, 'Session token not found!'));
+    res.send({});
   }
 };
 
