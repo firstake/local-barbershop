6a56a4d20e595337aaab4ad28daa6b98dc39d74b
Vladislav Tereshkin <firstaake@gmail.com>
2020-09-09 19:43:00 +0300

    server fixes via lint

--- server/index.js
+++ server/index.js
@@ -30,8 +30,8 @@ app.use('/api', appRoutes);
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 
-  app.use('*', (req, res) => {                       
-    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));                               
+  app.use('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
   });
 }
 
