76b2430b86357a2a9c7e49daa727d9281c042434
Vladislav Tereshkin <firstaake@gmail.com>
2020-09-18 18:35:25 +0300

    contacts tags

--- client/src/pages/Contacts.js
+++ client/src/pages/Contacts.js
@@ -16,24 +16,29 @@ const contacts = [
     text: `77
     Nowhere Avenue
     Planet Earth`,
+    textTag: 'address',
   },
   {
     icon: faPhone,
     color: '#4ccc4c',
     title: 'Phone',
     text: '8-800-555-35-35',
+    textTag: 'a',
+    href: 'tel: 88005553535',
   },
   {
     icon: faClock,
     color: '#da9171',
     title: 'Hours',
-    text: 'We are now opened every day! From 10:00 to 18:00',
+    text: `We are now opened every day! From <time>10:00</time> to <time>18:00</time>`,
   },
   {
     icon: faEnvelope,
     color: '#98cdfb',
     title: 'Email',
     text: 'info@localbarbershop.com',
+    textTag: 'a',
+    href: 'mailto:info@localbarbershop.com',
   },
 ];
 
@@ -46,31 +51,35 @@ class Contacts extends Component {
         </h1>
         <div className="row">
           {contacts.map(({
-            icon, color, title, text,
-          }) => (
-            <div className="col-lg-3 col-sm-6" key={title}>
-              <div
-                className="text-center
-                 border
-                 rounded
-                 d-flex
-                 justify-content-center
-                 align-items-center
-                 mt-3"
-                style={{minHeight: 250}}
-              >
-                <div className="px-3">
-                  <FontAwesomeIcon
-                    icon={icon}
-                    style={{fontSize: 50, color}}
-                    className="mb-2"
-                  />
-                  <h2>{title}</h2>
-                  <p className="text-break text-pre-line mb-0">{text}</p>
+            icon, color, title, text, textTag, href,
+          }) => {
+            const CustomTag = textTag || 'p';
+
+            return (
+              <div className="col-lg-3 col-sm-6" key={title}>
+                <div
+                  className="text-center border rounded d-flex
+                    justify-content-center align-items-center mt-3"
+                  style={{minHeight: 250}}
+                >
+                  <div className="px-3">
+                    <FontAwesomeIcon
+                      icon={icon}
+                      style={{fontSize: 50, color}}
+                      className="mb-2"
+                    />
+                    <h2>{title}</h2>
+                    <CustomTag
+                      className={`text-break text-pre-line mb-0${href ? ' text-inherit' : ''}`}
+                      dangerouslySetInnerHTML={{__html: text}}
+                      href={href || null}
+                    />
+                  </div>
                 </div>
               </div>
-            </div>
-          ))}
+            );
+          },
+          )}
         </div>
       </div>
     );
